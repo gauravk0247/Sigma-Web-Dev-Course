a85b72678637dfafc4a711eaac7e866542d83fbf
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-11-28 09:56:18 +0530

    lecture-55

---
+++ lecture-55/script.js
@@ -0,0 +1,44 @@
+console.log("This is our 55 video of sigma web development series");
+
+var a = 100;
+var _b = 20;
+// var 55d = 30; // Not Allowed
+console.log(a);
+console.log(_b);
+// console.log(55d);
+
+let aa = 20;
+aa = aa+50;
+console.log(aa);
+
+const ss = 23;
+// ss = 26; // Not Allowed we canot chnage beacause of const value
+console.log(ss);
+
+
+let x = 25;
+let y = "Hellioo";
+let z = null;
+let p = 34.56;
+let q = undefined;
+// let r = object;
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q);
+
+
+const item = {
+    "name": "Gaurav",
+    id : "201"
+}
+console.log(item);
+item.sal="234";
+console.log(item);
+
+
+// Quick Quiz 
+
+let student = {
+    name : "Gaurav",
+    "Phone Number": 1234567890,
+    marks: 78.89
+}
+console.log(student);
