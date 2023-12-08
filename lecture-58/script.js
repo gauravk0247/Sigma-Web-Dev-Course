08e3e56d27d96dc5c8861541f2b21282ace444e0
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-08 10:24:25 +0530

    lectute-58

---
+++ lecture-58/script.js
@@ -0,0 +1,14 @@
+function nice(name){
+    console.log("Hey "+name+" you are nice!");
+    console.log("Hey "+name+" you are good!");
+    console.log("Hey "+name+" you are nice!");
+    console.log("Hey "+name+" your tshirtis nice!");
+}
+nice("gaurav");
+nice("Krushna");
+
+
+function sum(a, b){
+    console.log(a+b);
+}
+sum(3, 5);
