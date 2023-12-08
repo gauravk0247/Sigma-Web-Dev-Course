6e8567aa5804dd10ce6b9f6d7d9602dd95fe9eca
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-08 10:27:25 +0530

    lectute-58

--- lecture-58/script.js
+++ lecture-58/script.js
@@ -8,7 +8,17 @@ nice("gaurav");
 nice("Krushna");
 
 
-function sum(a, b){
-    console.log(a+b);
+// function sum(a, b){
+//     console.log(a+b);
+// }
+// sum(3, 5);
+
+function sum(p, q) {
+    return p, q;
 }
-sum(3, 5);
+result = sum(10, 20);
+result1 = sum(20, 30);
+result2 = sum(1, 2);
+console.log("The result is "+ result);
+console.log("The result is "+ result1);
+console.log("The result is "+ result2);
