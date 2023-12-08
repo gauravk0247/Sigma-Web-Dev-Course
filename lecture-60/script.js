7d8cea9dcae4193e867045854d325d4cb7e434a1
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-08 10:39:38 +0530

    lectute-60

---
+++ lecture-60/script.js
@@ -0,0 +1,7 @@
+let a = "Gaurav";
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(a.toString());
+console.log(a.indexOf('u'));
+console.log(a.lastIndexOf('a'));
+console.log(a[0]);
