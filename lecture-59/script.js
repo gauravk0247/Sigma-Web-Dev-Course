819ef18537b14d76abf749b657f64fbe8c9e3227
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-08 10:35:38 +0530

    lectute-59

---
+++ lecture-59/script.js
@@ -0,0 +1,15 @@
+/*Create a faulty calculator using javascript
+
+this faulty calculator does following:
+1. It takes two number as input from the User.
+2. It performs wrong operation as follows:
+
+
++ -------> -
+* -------> +
+- ------> /
+/ ------> **
+
+
+It performs wrong opweration 10% of the times
+*/
