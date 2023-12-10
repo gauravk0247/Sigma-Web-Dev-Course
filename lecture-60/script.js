4031667c07eccdf5a103fb35bd78069d3e41e5fe
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-10 07:52:59 +0530

    lecture-60

--- lecture-60/script.js
+++ lecture-60/script.js
@@ -1,3 +1,4 @@
+console.log("This is our String tutorial.")
 let a = "Gaurav";
 console.log(a.toUpperCase());
 console.log(a.toLowerCase());
@@ -5,3 +6,26 @@ console.log(a.toString());
 console.log(a.indexOf('u'));
 console.log(a.lastIndexOf('a'));
 console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a.length);
+
+let name = "Developver";
+let job = "Java";
+console.log("His name is "+ name +" and his job is "+ job);
+console.log(`His name is ${name} and his job is ${job}`)
+
+let r = "Hello \"How are you?";
+console.log(r);
+console.log(a.slice(2, 5));
+console.log(a.slice(2));
+console.log(a.replace("G", "S"));
+console.log(a.split("a"));
+console.log(a.charAt(2));
+console.log(a.endsWith("G"));
+console.log(a.endsWith("v"));
+console.log(a.startsWith("G"));
+console.log(a.concat(" Hello"));
