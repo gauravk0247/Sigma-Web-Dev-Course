11dd3e7be92adca9b3ed7d8ecbe72657c2b6f152
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-12 21:26:54 +0530

    lecture-63

--- lecture-63/ArrayLoop.js
+++ lecture-63/ArrayLoop.js
@@ -1,5 +1,26 @@
 let a = [1, 45, 89, 2, 4, 7];
-for (let index = 0; index < a.length; index++) {
-    const element = array[index];
-    console.log(element);
-}
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     console.log(element);
+// }
+
+// a.forEach((value, index, arr)=>{
+//     console.log(value, index, arr)
+// })
+
+// let obj = {
+//     a:1,
+//     b:2,
+//     c:3
+// }
+
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(element);
+//     }
+// }
+
+// for (const iterator of a) {
+//     console.log(iterator);
+// }
