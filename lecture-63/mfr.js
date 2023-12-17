ba225a04d5557135c4b916c41bed2f9373bad7e5
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-17 16:47:11 +0530

    lecture-64

--- lecture-63/mfr.js
+++ lecture-63/mfr.js
@@ -1,7 +1,41 @@
 let arr = [1, 13, 5, 7, 11];
-let newarr = []
-for (let index = 0; index < arr.length; index++) {
-    const element = arr[index];
-    newarr.push(element**2);
-}
+// let newarr = []
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     newarr.push(element**2);
+// }
+// console.log(newarr);
+
+
+// Map() method
+let newarr = arr.map((e, index, Arrays)=>{
+    return e**2
+})
 console.log(newarr);
+
+
+// filter() method
+const greaterThanSeven = (e)=>{
+    if(e>7){
+        return true;
+    }
+    return false;
+}
+console.log(newarr.filter(greaterThanSeven));
+
+
+// filter() method optimized code
+// console.log(arr.filter((e)=>{
+//     if(e<7){
+//         return true;
+//     }
+//     return false;
+// }))
+
+let arr2 = [1, 2, 3, 4, 5, 6];
+const red = (a, b) =>{
+    return a*b;
+}
+console.log(arr2.reduce(red));
+
+console.log(Array.from("Gaurav"));
