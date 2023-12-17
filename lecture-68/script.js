dbbdfaae95fd6472b16bbe899dd853a6ebd5125d
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-17 18:45:19 +0530

    lecture-68

---
+++ lecture-68/script.js
@@ -0,0 +1,21 @@
+console.log("Hello World")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes);
+
+// boxes[2].style.backgroundColor = "red";
+
+// document.getElementById("redbox").style.backgroundColor = "red";
+
+
+// it will change 1st color
+// document.querySelector(".box").style.backgroundColor = "green";
+
+// for all change the color we used the querySelectorAll
+
+// console.log(document.querySelectorAll(".box")); // it is not work
+
+document.querySelectorAll(".box").forEach(e =>{
+    e.style.backgroundColor = "green";
+    e.style.color="white";
+})
