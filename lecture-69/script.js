231a811bdec04a1a9f93633566639213a87a906e
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-19 19:15:00 +0530

    lecture-69

---
+++ lecture-69/script.js
@@ -0,0 +1,30 @@
+let a = 6;
+
+function factorial(number){
+    let arr = Array.from(Array(number+1).keys())
+    console.log(arr.slice(1, ));
+    let c = arr.slice(1, ).reduce((a, b)=>{
+        return a*b;
+    })
+    console.log(c);
+}
+factorial(a);
+
+// function factorial(number){
+//     let arr = Array.from(Array(number+1).keys())
+//     let c = arr.slice(1, ).reduce((a, b)=> a*b )
+//     return c;
+// }
+// factorial(a);
+
+
+// Normal Approach
+
+function facFor(number){
+    let fac = 1;
+    for (let index = 1; index <= number; index++) {
+        fac = fac * index;
+    }
+    return fac;
+}
+console.log(facFor(a));
