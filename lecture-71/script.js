6e51b5f06f18125f9eb982a30c58cc1424161c64
gauravk0247 <gauravkhairnar2106@gmail.com>
2024-01-13 12:46:24 +0530

    lecture-71

---
+++ lecture-71/script.js
@@ -0,0 +1,11 @@
+document.querySelector(".box");
+document.querySelector(".box").innerHTML
+document.querySelector(".container").innerHTML
+document.querySelector(".container").tagName
+document.querySelector(".container").nodeName
+document.querySelector(".container").textContent
+document.querySelector(".container").hidden
+document.querySelector(".container").hidden = true
+document.querySelector(".box").innerHTML = "Hey i am new text"
+// document.querySelector(".box").hasAttribute("style")
+document.querySelector(".box").getAttribute("style")
