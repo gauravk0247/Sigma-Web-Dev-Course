04de035779dca48a841e0facb97fe73f57098e23
gauravk0247 <gauravkhairnar2106@gmail.com>
2024-01-14 12:27:13 +0530

    lecture-75

--- lecture-75/promise.js
+++ lecture-75/promise.js
@@ -11,8 +11,30 @@ let prom1 = new Promise((resolve, reject)=>{
         }, 3000);
         }
 })
-prom1.then((a)=>{
-    console.log(a);
-}).catch((err)=>{
-    console.log(err);
+
+let prom2 = new Promise((resolve, reject)=>{
+    let a = Math.random();
+    if(a<0.5){
+        reject("No random number was not supporting you 2")
+    }else{
+            setTimeout(()=>{
+            console.log("Yes I am done 2");
+            resolve("Gaurav 2");
+        }, 1000);
+        }
+})
+
+// prom1.then((a)=>{
+//     console.log(a);
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+// let p3 = Promise.all([prom1, prom2])
+// let p3 = Promise.allSettled([prom1, prom2])
+let p3 = Promise.race([prom1, prom2])
+p3.then((a)=>{
+    console.log(a)
+}).catch(err=>{
+    console.log(err)
 })
