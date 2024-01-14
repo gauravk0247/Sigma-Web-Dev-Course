03d5a45c417b945b761deab8d077e280a5e0b7a2
gauravk0247 <gauravkhairnar2106@gmail.com>
2024-01-14 09:32:11 +0530

    lecture-73

---
+++ lecture-73/script.js
@@ -0,0 +1,5 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail){
+    // finish this function
+}
+
+createCard("Introduction to Backend | Sigma Web Dev Video #2", "CodeWithHarry", 560000, 7, "31:22","https://i.ytimg.com/vi/kJEsTjH5mVg/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBTa4i6NuryzZhcPo7BkCK1G6PqGg")
