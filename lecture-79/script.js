0b3cc76ef242b34930cfd195268e8d0a07fccdca
gauravk0247 <gauravkhairnar2106@gmail.com>
2024-01-16 15:10:06 +0530

    lecture-79

--- lecture-79/script.js
+++ lecture-79/script.js
@@ -5,10 +5,18 @@ if(isNaN(a) || isNaN(b)){
     throw SyntaxError("Sorry this is not allowed")
 }
 let sum = parseInt(a)+parseInt(b)
-
+function main(){
+    let x = 1;
 try {
     console.log("The sum is ", sum*x);
+    return true;
     
 } catch (error) {
     console.log("Error aa gaya bhai");
+    return false;
 }
+finally{
+    console.log("Flies are being closed and db connection is being closed");
+}
+}
+let c = main()
