f210863f4bc426e827f2f6e7977b75f45928c2f0
gauravk0247 <gauravkhairnar2106@gmail.com>
2024-01-19 11:01:03 +0530

    lecture-82

---
+++ lecture-82/script.js
@@ -0,0 +1,9 @@
+async function sleep(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            resolve(45);
+        }, 1000);
+    })
+}
+let a = await sleep()
+let b = await sleep()
